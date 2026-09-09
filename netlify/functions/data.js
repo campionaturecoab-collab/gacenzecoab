862c76622bd3b25ab2f093ccf527ed225a3fdeea
campionaturecoab-collab <campionaturecoab@gmail.com>
2026-09-09 12:53:02 +0200

    fix

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,6 +1,7 @@
-const { getStore } = require("@netlify/blobs");
+const { getStore, connectLambda } = require("@netlify/blobs");
 
 exports.handler = async (event) => {
+  connectLambda(event);
   const store = getStore("magazzino");
 
   const headers = {
